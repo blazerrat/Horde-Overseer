5f1287ecb07aa21e462abfdf20de93e20d6f4c6a
blazerrat <fireprooftoptucker@gmail.com>
2023-05-03 23:55:49 +0530

    KAI horde dashboard MVP ready

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ const submitUID = document.getElementById("submitUid");
 const sidebar = document.getElementById("sidebar");
 const sidebarInfo = document.getElementById("sidebarInfo");
 const signIn = document.getElementById("signIn");
-const leaderboard = document.getElementById("leaderboard");
+//const leaderboard = document.getElementById("leaderboard");
+const modelsStats = document.getElementById("modelsStats");
 
 // Option Elements
 const uidAlt = document.getElementById("uidAlt");
 const submitUIDAlt = document.getElementById("submitUidAlt");
-const leaderboardOption = document.getElementById("leaderboardOption");
+//const leaderboardOption = document.getElementById("leaderboardOption");
+const modelsStatsOption = document.getElementById("modelsStatsOption");
 const lightModeOption = document.getElementById("lightmode");
 const sortWorkersOption = document.getElementById("sortWorkers");
 
@@ -21,7 +23,8 @@ let cachedWorkers = [];
 let options = {
     userID: localStorage.getItem('userID') || -1,
     lightMode: (localStorage.getItem('lightMode') === 'true') || false,
-    leaderboardEnabled: (localStorage.getItem('leaderboardEnabled') === 'true') || true,
+    //leaderboardEnabled: (localStorage.getItem('leaderboardEnabled') === 'true') || true,
+    modelsStatsEnabled: (localStorage.getItem('modelsStatsEnabled') === 'true') || true,
     sortWorkers: localStorage.getItem('sortWorkers') || 'none'
 };
 
@@ -37,9 +40,14 @@ if (options.lightMode) {
     document.querySelector(".close-modal>svg>path").setAttribute("fill", "rgb(111, 111, 111)");
 }
 
-if (options.leaderboardEnabled) {
+/* if (options.leaderboardEnabled) {
     leaderboardOption.checked = true;
     leaderboard.style.display = "table";
+} */
+
+if (options.modelsStatsEnabled) {
+    modelsStatsOption.checked = true;
+    modelsStats.style.display = "table";
 }
 
 sortWorkersOption.value = options.sortWorkers;
@@ -58,7 +66,7 @@ function secondsToDhm(seconds) {
 }
 
 submitUID.addEventListener("click", async () => {
-    const response = await fetch("https://stablehorde.net/api/v2/users/" + uid.value);
+    const response = await fetch("https://horde.koboldai.net/api/v2/users/" + uid.value);
     if (!response.ok) return;
 
     options.userID = uid.value;
@@ -69,7 +77,7 @@ submitUID.addEventListener("click", async () => {
 })
 
 submitUIDAlt.addEventListener("click", async () => {
-    const response = await fetch("https://stablehorde.net/api/v2/users/" + uidAlt.value);
+    const response = await fetch("https://horde.koboldai.net/api/v2/users/" + uidAlt.value);
     if (!response.ok) return;
 
     options.userID = uidAlt.value;
@@ -81,28 +89,28 @@ submitUIDAlt.addEventListener("click", async () => {
 async function updateSidebarInfo() {
     if (options.userID === -1) return;
 
-    let response = await fetch("https://stablehorde.net/api/v2/users/" + options.userID);
+    let response = await fetch("https://horde.koboldai.net/api/v2/users/" + options.userID);
     if (!response.ok) return;
     let user = await response.json();
 
-    let statsResponse = await fetch("https://stablehorde.net/api/v2/status/performance");
+    let statsResponse = await fetch("https://horde.koboldai.net/api/v2/status/performance");
     if (!statsResponse.ok) return;
     let stats = await statsResponse.json();
 
     sidebarInfo.innerHTML = `
         <h1>Welcome back, ${user.username}</h1>
         <div>You have <b>${user.kudos}</b> kudos remaining.</div>
-        <div>You have contributed <b>${user.contributions.megapixelsteps}</b> MPS and fulfilled <b>${user.contributions.fulfillments} </b>requests.</div>
+        <div>You have contributed <b>${user.records.contribution.tokens}</b> tokens and fulfilled <b>${user.records.fulfillment.text} </b>requests.</div>
         <br>
-        <div>There are <b>${stats.queued_requests}</b> queued requests (<b>${stats.queued_megapixelsteps}</b> MPS) with <b>${stats.worker_count}</b> workers.</div>
-        <div>In the past minute, there have been <b>${stats.past_minute_megapixelsteps}</b> MPS processed.</div>
+        <div>There are <b>${stats.queued_text_requests}</b> queued requests (<b>${stats.queued_tokens}</b> tokens) with <b>${stats.text_worker_count}</b> workers.</div>
+        <div>In the past minute, there have been <b>${stats.past_minute_tokens}</b> tokens processed.</div>
     `;
 }
 
 // Can only call once per 30s
-async function updateLeaderboard() {
+/* async function updateLeaderboard() {
     if (!options.leaderboardEnabled) return;
-    let response = await fetch("https://stablehorde.net/api/v2/users");
+    let response = await fetch("https://horde.koboldai.net/api/v2/users");
     if (!response.ok) return;
     let users = await response.json();
 
@@ -124,6 +132,41 @@ async function updateLeaderboard() {
             </tr>
         `;
     }
+} */
+
+// Can only call once per 30s
+async function updateModelsStats() {
+    if (!options.modelsStatsEnabled) return;
+    let response = await fetch("https://horde.koboldai.net/api/v2/status/models?type=text");
+    if (!response.ok) return;
+    let models = await response.json();
+
+    models.sort((a, b) => b.queued - a.queued);
+    modelsStats.innerHTML = `
+        <tr>
+            <th>#</th>
+            <th>Model</th>
+            <th>Count</th>
+            <th>ETA</th>
+            <th>Jobs</th>
+            <th>Queue</th>
+            <th>T/s</th>
+        </tr>
+    `;
+    for (let i = 0; i < 10; i++) {
+        const model = models[i];
+        modelsStats.innerHTML += `
+            <tr>
+                <td>${i+1}</td>
+                <td>${model.name}</td>
+                <td>${model.count}</td>
+                <td>${model.eta}</td>
+                <td>${model.jobs}</td>
+                <td>${model.queued}</td>
+                <td>${model.performance}</td>
+            </tr>
+        `;
+    }
 }
 
 // Does not work with functions, classes, undefined, Infinity, RegExps, Maps, Sets
@@ -136,7 +179,7 @@ async function updateWorkers(useCached = false) {
     if (useCached) {
         workers = deepClone(cachedWorkers);
     } else {
-        const response = await fetch("https://stablehorde.net/api/v2/workers");
+        const response = await fetch("https://horde.koboldai.net/api/v2/workers?type=text");
         if (!response.ok) return;
         workers = await response.json();
     }
@@ -149,24 +192,24 @@ async function updateWorkers(useCached = false) {
         workers.sort((a, b) => b.kudos_rewards - a.kudos_rewards);
     } else if (options.sortWorkers == "reqfilled") {
         workers.sort((a, b) => b.requests_fulfilled - a.requests_fulfilled);
-    } else if (options.sortWorkers == "totalmps") {
-        workers.sort((a, b) => b.megapixelsteps_generated - a.megapixelsteps_generated);
-    } else if (options.sortWorkers == "maxsize") {
-        workers.sort((a, b) => b.max_pixels - a.max_pixels);
+    } else if (options.sortWorkers == "contextlength") {
+        workers.sort((a, b) => b.max_context_length - a.max_context_length);
+    } else if (options.sortWorkers == "maxlength") {
+        workers.sort((a, b) => b.max_length - a.max_length);
     }
 
     content.innerHTML = "";
     for (let j = 0; j < workers.length; j++) {
         const worker = workers[j];
         const person = document.createElement("div");
-        const max_size = Math.round(Math.sqrt(worker.max_pixels));
+        //const max_size = Math.round(Math.sqrt(worker.max_pixels));
         person.innerHTML = `
             ==================<br><br>
             <div style="font-size:20px;font-weight:800">${worker.name} - ${secondsToDhm(worker.uptime)}</div>
             <div style="font-size:12px; margin-bottom: 10px">ID: ${worker.id}</div>
-            <div>Max Size: ${max_size}x${max_size}</div>
-            <div>Requests Fulfilled: ${worker.requests_fulfilled}</div>
-            <div>MPS Generated: ${worker.megapixelsteps_generated} - ${worker.performance.split(" ")[0]} MPS/s</div>
+            <div>Max Length: ${worker.max_length}</div>
+            <div>Max Context Length: ${worker.max_context_length}</div>
+            <div>Requests Fulfilled: ${worker.requests_fulfilled} - ${worker.performance.split(" ")[0]} Tokens/s</div>
             <div>Kudos Gained: ${worker.kudos_rewards}</div>
             <ul style="margin: 0"><li>From Generating: ${worker.kudos_details.generated}</li><li>From Uptime: ${worker.kudos_details.uptime}</li></ul><br>
             ==================<br>
@@ -197,7 +240,7 @@ lightModeOption.addEventListener("change", () => {
     document.querySelector(".close-modal>svg>path").setAttribute("fill", "#909090");
 })
 
-leaderboardOption.addEventListener("change", () => {
+/* leaderboardOption.addEventListener("change", () => {
     options.leaderboardEnabled = leaderboardOption.checked;
     localStorage.setItem("leaderboardEnabled", options.leaderboardEnabled);
     
@@ -206,6 +249,17 @@ leaderboardOption.addEventListener("change", () => {
         return;
     }
     leaderboard.style.display = "none";
+}) */
+
+modelsStatsOption.addEventListener("change", () => {
+    options.modelsStatsEnabled = modelsStatsOption.checked;
+    localStorage.setItem("modelsStatsEnabled", options.modelsStatsEnabled);
+    
+    if (options.modelsStatsEnabled) {
+        modelsStats.style.display = "table";
+        return;
+    }
+    modelsStats.style.display = "none";
 })
 
 sortWorkersOption.addEventListener("change", () => {
@@ -215,7 +269,7 @@ sortWorkersOption.addEventListener("change", () => {
     updateWorkers(true);
 })
 
-function moveLeaderboard() {
+/* function moveLeaderboard() {
     if (window.innerWidth < 650) {
         if (leaderboard.parentElement.id != 'content') {
             content.parentElement.prepend(leaderboard);
@@ -225,14 +279,29 @@ function moveLeaderboard() {
     if (leaderboard.parentElement.id != 'sidebar') {
         sidebar.appendChild(leaderboard);
     }
+} */
+
+function moveModelsStats() {
+    if (window.innerWidth < 650) {
+        if (modelsStats.parentElement.id != 'content') {
+            content.parentElement.prepend(modelsStats);
+        }
+        return;
+    }
+    if (modelsStats.parentElement.id != 'sidebar') {
+        sidebar.appendChild(modelsStats);
+    }
 }
 
-window.addEventListener('resize', moveLeaderboard);
+//window.addEventListener('resize', moveLeaderboard);
 
-moveLeaderboard();
+//moveLeaderboard();
+moveModelsStats();
 updateSidebarInfo();
 updateWorkers();
-updateLeaderboard();
-setInterval(updateWorkers, 1000 * 5);
-setInterval(updateSidebarInfo, 1000 * 5);
-setInterval(updateLeaderboard, 1000 * 60);
+//updateLeaderboard();
+updateModelsStats();
+setInterval(updateWorkers, 1000 * 10);
+setInterval(updateSidebarInfo, 1000 * 10);
+//setInterval(updateLeaderboard, 1000 * 60);
+setInterval(updateModelsStats, 1000 * 60);
